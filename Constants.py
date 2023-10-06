368409954fe23417423462594b3fbe2152879867
chalie <formathead79@gmail.com>
2023-10-06 10:48:32 +0900

    >> 1) Gimbal Start >> 2) Mouse Control >> 3) Login >> 4) Menu Move >> 5) save excel type after capture >> 6) OCR

---
+++ Constants.py
@@ -0,0 +1,3 @@
+import pywinauto
+
+main_Dig_Coordinate = [2240, 165, 3520, 885]
---
+++ Login_MainFrame.py
@@ -0,0 +1,117 @@
+import time
+import numpy as np
+
+import pytesseract
+import xlsxwriter
+import pyocr
+import cv2
+
+import Constants
+
+from pywinauto import mouse
+from PIL import Image
+
+
+def login_correct_WithOnlyPW(dia, pw):
+    # dia['passwordEdit'].type_keys(f'{pw}')
+    # dia['OK'].click()
+    dia.passwordEdit.type_keys(pw)
+    time.sleep(1)
+    dia['OK'].click()
+
+
+def capture_Menu(dia, menus):
+    workbook = xlsxwriter.Workbook('Menu Capture.xlsx')
+    worksheet = workbook.add_worksheet()
+    row_position = 0
+
+    for row_num, menu in enumerate(menus):
+        row_num = row_position
+        dia.child_window(title=f'{menu}', control_type='CheckBox').click_input()
+        dia.capture_as_image().save(f'Capture/{menu}.png')
+        worksheet.write(row_num, 0, menu)
+        worksheet.insert_image(row_num + 1, 0, f'Capture/{menu}.png')
+
+        time.sleep(1)
+        row_position += 38
+
+    workbook.close()
+
+
+def cal_element_coordinate(x, y, w, h):
+    ele_coord = {'x': 0,
+                 'y': 0,
+                 'w': 0,
+                 'h': 0}
+    coordinate_array = Constants.main_Dig_Coordinate
+    ele_coord['x'] = x - coordinate_array[0]
+    ele_coord['y'] = y - coordinate_array[1]
+    ele_coord['w'] = w - coordinate_array[0]
+    ele_coord['h'] = h - coordinate_array[1]
+
+    return ele_coord
+
+
+# HSV 색상(Hue), 채도(Saturation), 명도(Value)의 좌표
+def image_to_string_with_hsk(image_path):
+    # Load The Image
+    read_img = cv2.imread(fr'{image_path}')
+
+    # Up-Sample
+    read_img = cv2.resize(read_img, (0, 0), fx=2, fy=2)
+
+    # Convert to HSV
+    hsv = cv2.cvtColor(read_img, cv2.COLOR_BGR2HSV)
+
+    # Get Binary Mask
+    msk = cv2.inRange(hsv, np.array([0, 0, 123]), np.array([179, 255, 255]))
+    txt_return = pytesseract.image_to_string(msk)
+
+    return txt_return
+
+
+def image_to_string_with_lim(image_path):
+
+    # Load The Image
+    read_img = cv2.imread(fr'{image_path}')
+
+    # Convert To Gray
+    gray = cv2.cvtColor(read_img, cv2.COLOR_BGR2GRAY)
+
+    # Use Limit Value
+    gray = cv2.threshold(gray, 0, 255,
+                         cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+    txt_return = pytesseract.image_to_string(gray)
+    print(txt_return)
+
+    return txt_return
+
+
+def image_to_string_with_blur(image_path):
+    # Load The Image
+    read_img = cv2.imread(fr'{image_path}')
+
+    # Convert To Gray
+    gray = cv2.cvtColor(read_img, cv2.COLOR_BGR2GRAY)
+
+    # Use Blur
+    blur = cv2.medianBlur(gray, 9)
+
+    txt_return = pytesseract.image_to_string(blur)
+
+    return txt_return
+
+
+def generate_txt_array_with_img(processed_img, txt_title):
+    note_memo = open(fr'Text\{txt_title}.txt', 'w+')
+    for i in processed_img:
+        note_memo.write(i)
+    note_memo.close()
+
+    note_memo = open(fr'Text\{txt_title}.txt', 'r+')
+    note_memo = note_memo.readlines()
+    note_memo = [line.strip() for line in note_memo]
+    note_memo = [v for v in note_memo if v]
+
+    return note_memo
